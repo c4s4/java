1527fbc5d39e1bc40697ae1f91365ce1f60c19d9
Michel Casabianca <casa@sweetohm.net>
2017-12-01 00:20:16 +0100

    Added Junit tests

---
+++ test/casa/MainTest.java
@@ -0,0 +1,12 @@
+package casa;
+
+import junit.framework.*;
+ 
+public class MainTest
+    extends TestCase {
+ 
+    public void testNothing()
+        throws Exception {
+        assertTrue(true);
+    }
+}
